1ed864618f79bda108e761ed826ac5cab1495b0c
ksballetba <2501226111@qq.com>
2020-06-11 14:28:22 +0800

    1.基础接口适配新版Api

---
+++ app/src/main/java/com/ksballetba/rayplus/data/bean/NavigationBean.kt
@@ -0,0 +1,16 @@
+package com.ksballetba.rayplus.data.bean
+
+
+import com.google.gson.annotations.SerializedName
+
+data class NavigationBean(
+    val code: Int, // 200
+    val `data`: List<Data>,
+    val msg: String // success
+) {
+    data class Data(
+        @SerializedName("cycle_number")
+        val cycleNumber: Int, // 2
+        val title: String // 访视2
+    )
+}
